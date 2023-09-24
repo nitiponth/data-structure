28f4a926e94ce52c02eaedd062565783eb8da8c5
Ice Nitipon <nitipon.t@techcombine.co>
2023-09-25 00:31:09 +0700

    :sparkles: Add findLongestSubstring assignment

---
+++ src/problem-solving-patterns/findLongestSubstring.ts
@@ -0,0 +1,17 @@
+export const findLongestSubstring = (str: string) => {
+  if (!str.length) return 0;
+
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    longest = Math.max(longest, i - start + 1);
+    seen[char] = i + 1;
+  }
+  return longest;
+};
---
+++ test/findLongestSubstring.test.ts
@@ -0,0 +1,27 @@
+import { expect, test } from "vitest";
+import { findLongestSubstring } from "../src/problem-solving-patterns/findLongestSubstring";
+
+test("test countUniqueValues by ''", () => {
+  expect(findLongestSubstring("")).toBe(0);
+});
+test("test countUniqueValues by 'thisisawesome'", () => {
+  expect(findLongestSubstring("thisisawesome")).toBe(6);
+});
+
+test("test countUniqueValues by 'rithmschool'", () => {
+  expect(findLongestSubstring("rithmschool")).toBe(7);
+});
+
+test("test countUniqueValues by 'thecatinthehat'", () => {
+  expect(findLongestSubstring("thecatinthehat")).toBe(7);
+});
+
+test("test countUniqueValues by 'bbbbbb'", () => {
+  expect(findLongestSubstring("bbbbbb")).toBe(1);
+});
+test("test countUniqueValues by 'longestsubstring'", () => {
+  expect(findLongestSubstring("longestsubstring")).toBe(8);
+});
+test("test countUniqueValues by ''thisishowwedoit'", () => {
+  expect(findLongestSubstring("'thisishowwedoit")).toBe(6);
+});
